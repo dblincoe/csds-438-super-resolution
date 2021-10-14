18708dd961bb62a829cfd95441b5a201ad81716a
dblincoe <dblincoe@gmail.com>
2021-10-14 03:10:18 -0400

    Downsampler works

--- data/data_utils.py
+++ data/data_utils.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import numpy as np
+from cv2 import cv2
 
 
 def downsample_images(images: List[np.array], factor: int) -> List[np.array]:
@@ -13,4 +14,10 @@ def downsample_images(images: List[np.array], factor: int) -> List[np.array]:
     Returns:
         List[np.array]: downsampled images
     """
-    pass
+    lr_images = []
+
+    for image in images:
+        lr_image = cv2.resize(image, (int(image.shape[1] / factor), int(image.shape[0] / factor)))
+        lr_images.append(lr_image)
+
+    return lr_images
